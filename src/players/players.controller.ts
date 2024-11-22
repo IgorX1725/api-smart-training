e714c64bc905a19d031d5504d7d8d061784450c3
Igor Cortez <ibarbos2@its.jnj.com>
2024-11-22 14:20:29 -0300

    Updating player controller

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePlayerDto } from './dtos/createPlayer.dto';
 
 @Controller('api/v1/players')
 export class PlayersController {
   @Post()
-  async CreateUpdatePlayer() {
+  async CreateUpdatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
+    const { email } = createPlayerDto;
     return JSON.stringify({
-      name: 'John Doe',
+      name: email,
     });
   }
 }
